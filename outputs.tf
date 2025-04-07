4d5d2d03cf00d53262ca5932b7e56fcef39a4de1
Ege Yeşilbaş <162204375+EgeYe61@users.noreply.github.com>
2025-04-07 11:07:36 +0300

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "cluster_name" {
-  value = google_container_cluster.primary.name
-}
-
-output "kubeconfig_command" {
-  value = "gcloud container clusters get-credentials ${google_container_cluster.primary.name} --region=${var.region} --project=${var.project_id}"
-}
