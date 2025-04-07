c15145cf90e2376fb7eefa3de2ea476b0431bf5d
Ege Yeşilbaş <162204375+EgeYe61@users.noreply.github.com>
2025-04-07 10:25:14 +0300

    Add files via upload

---
+++ kubernetes-use-case/use-case-terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "gke-case-study"
+  type        = string
+  default     = "gke-case-study"
+}
+
+variable "region" {
+  description = "Region"
+  type        = string
+  default     = "europe-west1"
+}
